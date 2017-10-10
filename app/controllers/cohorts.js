32596af3fb0fbcb5f6cd9baf479ae349b8a4779c
Chris Swires <csw@ihtsdo.org>
2017-10-10 15:01:57 +0100

    Removal of null fields on searches, correctly display totals, alternate spinner

--- app/controllers/cohorts.js
+++ app/controllers/cohorts.js
@@ -30,39 +30,49 @@ export default Ember.Controller.extend({
             if(this.get('model.gender') !== null && this.get('model.gender') !== undefined){
                 gender = this.get('model.gender').toUpperCase();
             }
-            let ageMin = this.get('model.ageMin');
-            let ageMax = this.get('model.ageMax');
 
             let inclusionCriteria = this.get('model.inclusionCriteria');
             console.log("inclusionCriteria = " + inclusionCriteria);
 
             let inclusionCriteriaData = null;
+            let postData = null;
             if (Ember.isPresent(inclusionCriteria)) {
                 if(inclusionCriteria.indexOf("<<") === -1 && inclusionCriteria.indexOf(">>") === -1){
                     inclusionCriteria = "<< " + inclusionCriteria;
                 }
                 inclusionCriteriaData = {
                     ecl: this.toECL(inclusionCriteria),
-                    includeDaysInPast: this.get('model.includeDaysInPast'),
-                    includeDaysInFuture: this.get('model.includeDaysInFuture')
                 };
+                if(this.get('model.includeDaysInPast') !== null && this.get('model.includeDaysInPast') !== undefined){
+                    inclusionCriteriaData.includeDaysInPast = this.get('model.includeDaysInPast');
+                }
+                if(this.get('model.includeDaysInFuture') !== null && this.get('model.includeDaysInFuture') !== undefined){
+                    inclusionCriteriaData.includeDaysInFuture = this.get('model.includeDaysInPast');
+                }
             }
-
             console.log("fetch cohort for " + primaryExposureECL + " with inclusionCriteria" + inclusionCriteriaData);
             if(primaryExposure.indexOf("<<") === -1 && primaryExposure.indexOf(">>") === -1){
                 primaryExposure = "<< " + primaryExposure;
             }
-            this.get('ajax').post('/health-analytics-api/cohorts/select', {
-                contentType: 'application/json; charset=utf-8',
-                data: JSON.stringify({
-                    gender: gender,
-                    minAge: ageMin,
-                    maxAge: ageMax,
+            postData = {
                     primaryExposure:  {
                         "ecl": primaryExposure
                       },
                     inclusionCriteria: inclusionCriteriaData
-                })})
+                };
+            if(this.get('model.gender') !== null && this.get('model.gender') !== undefined){
+                postData.gender = this.get('model.gender').toUpperCase();
+            }
+            if(this.get('model.ageMin') !== null && this.get('model.ageMin') !== undefined){
+                postData.minAge = this.get('model.ageMin');
+            }
+            if(this.get('model.ageMax') !== null && this.get('model.ageMax') !== undefined){
+                postData.maxAge = this.get('model.ageMax');
+            }
+            var data = 
+            this.get('ajax').post('/health-analytics-api/cohorts/select', {
+                contentType: 'application/json; charset=utf-8',
+                data: JSON.stringify(postData)})
                 .then((result) => {
                     this.set('loading', false);
                     this.set('model.cohortData', result);
