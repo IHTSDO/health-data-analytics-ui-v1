95c6e87b94914d133afa9c122c25a8e606b09ae6
Chris Swires <csw@ihtsdo.org>
2017-11-01 16:34:19 +0000

    End to end creation of subset and successful use in the cohorts page

--- app/controllers/subsets/edit.js
+++ app/controllers/subsets/edit.js
@@ -20,28 +20,98 @@ export default Ember.Controller.extend({
             console.log("ECL - " + ecl);
             this.set("model.ecl", ecl);
         },
-        addFocusConcept() {
-            this.get("model.eclObjects.focusConcepts").pushObject({conceptId: ""});
+        addEclObject() {
+            this.get("model.eclObjects").pushObject(
+                {
+                    "operator": "OR",
+                    "ecl": "<<",
+                    "focusConcept": {
+                        "id": "",
+                        "fsn": "Any"
+                    },
+                    "refinements": [{
+                        "type": {
+                            "id": "",
+                            "fsn": "Any",
+                            "ecl": "<<"
+                        },
+                        "target": {
+                            "id": "",
+                            "fsn": "Any",
+                            "ecl": "<<"
+                        }
+                    }]
+
+                }
+            );
+        },
+        addRefinement(model){
+            model.pushObject(
+                {
+                    "type": {
+                        "id": "",
+                        "fsn": "Any",
+                        "ecl": "<<"
+                    },
+                    "target": {
+                        "id": "",
+                        "fsn": "Any",
+                        "ecl": "<<"
+                    }
+                }
+            );
         },
         saveSubset() {
-            console.log("saveSubset");
-            console.log(this.get("model"));
-//            this.set('saving', true);
-//            this.get('ajax').post('/health-analytics-api/subsets',
-//                {
-//                    contentType: 'application/json; charset=utf-8',
-//                    data: JSON.stringify(this.get("model"))
-//                })
-//                .then((result) => {
-//                    this.set('saving', false);
-//                    this.set('model', result);
-//                });
+            var ecl = "";
+            var eclObjects = this.get("model.eclObjects");
+            eclObjects.forEach(function(item){
+                if(item.refinements.length > 0)
+                    {
+                        ecl = ecl + "(";
+                    }
+                ecl = ecl + " " + item.operator + " ";
+                ecl = ecl + item.ecl + " ";
+                ecl = ecl + item.focusConcept.id;
+                item.refinements.forEach(function(refinement){
+                    if(refinement === item.refinements[0])
+                        {
+                            ecl = ecl + " : ";
+                        }
+                    else{
+                            ecl = ecl + " , ";
+                        }
+                    ecl = ecl + refinement.type.ecl + " ";
+                    ecl = ecl + refinement.type.id + " = ";
+                    ecl = ecl + refinement.target.ecl + " ";
+                    ecl = ecl + refinement.target.id;
+                });
+                if(item.refinements.length > 0)
+                    {
+                        ecl = ecl + ")";
+                    }
+            });
+            
+            
+            this.set('saving', true);
+            var postData = {
+                description : this.get("model.name"),
+                name : this.get("model.name"),
+                eclModel : JSON.stringify(this.get("model.eclObjects")),
+                ecl : ecl
+            }
+            this.get('ajax').post('/health-analytics-api/subsets',
+                {
+                    contentType: 'application/json; charset=utf-8',
+                    data: JSON.stringify(postData)
+                })
+                .then((result) => {
+                    this.set('saving', false);
+                });
         },
-          setValue(model, event) {
+          updateValue(model) {
+               console.log(model);
             model = event.target.value;
+              console.log(this.get("model"));
           },
-        loadValue(model, event) {
-            event.target.value = model;
-          }
     }
 });
