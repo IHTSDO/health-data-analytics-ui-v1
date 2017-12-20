3f0da678c18bd1a70610f41f7584a95dde463afe
Kai Kewley <kaikewley@gmail.com>
2017-12-20 16:57:10 +0000

    Multiple wording fixes for cohort statistical test report.

--- app/helpers/get-wording.js
+++ app/helpers/get-wording.js
@@ -1,51 +1,56 @@
 import Ember from 'ember';
 
 export function getWording(typeId) {
-        if(typeId[0] === "64572001" && typeId[1] === "past")
+        var disorder = "64572001";
+        var product = "373873005";
+        var procedure = "71388002";
+        var conceptType = typeId[0];
+        var useType = typeId[1];
+        if(conceptType === disorder && useType === "past")
             {
                 return "had";
             }
-        else if(typeId[0] === "64572001" && typeId[1] === "and")
+        else if(conceptType === disorder && useType === "and")
             {
-                return "had developed";
+                return "developed";
             }
-        else if(typeId[0] === "64572001" && typeId[1] === "present")
+        else if(conceptType === disorder && useType === "present")
             {
                 return "have";
             }
-        else if(typeId[0] === "64572001" && typeId[1] === "verb")
+        else if(conceptType === disorder && useType === "verb")
             {
                 return "developing";
             }
-        else if(typeId[0] === "373873005" && typeId[1] === "past")
+        else if(conceptType === product && useType === "past")
             {
                 return "took";
             }
-        else if(typeId[0] === "373873005" && typeId[1] === "and")
+        else if(conceptType === product && useType === "and")
             {
                 return "had taken";
             }
-        else if(typeId[0] === "373873005" && typeId[1] === "present")
+        else if(conceptType === product && useType === "present")
             {
                 return "take";
             }
-        else if(typeId[0] === "373873005" && typeId[1] === "verb")
+        else if(conceptType === product && useType === "verb")
             {
                 return "taking";
             }
-        else if(typeId[0] === "71388002" && typeId[1] === "past")
+        else if(conceptType === procedure && useType === "past")
             {
                 return "had undergone";
             }
-        else if(typeId[0] === "71388002" && typeId[1] === "and")
+        else if(conceptType === procedure && useType === "and")
             {
                 return "also underwent";
             }
-        else if(typeId[0] === "71388002" && typeId[1] === "present")
+        else if(conceptType === procedure && useType === "present")
             {
                 return "underwent";
             }
-        else if(typeId[0] === "71388002" && typeId[1] === "verb")
+        else if(conceptType === procedure && useType === "verb")
             {
                 return "undergoing";
             }
