0d87609f7f7408fddea61a383c3fa34625a43a3b
Chris Swires <csw@ihtsdo.org>
2018-10-16 14:06:09 +0100

    Added a typeahead dealy to subset concept typeaheads

--- app/components/subset-concept-listing.js
+++ app/components/subset-concept-listing.js
@@ -31,117 +31,124 @@ export default Ember.Component.extend({
     },
     actions: {
         autoComplete(param) {
+            var run = function(scope) {
+                    if(param !== "" && param !== '*' && scope.get('mrcmType') === null && (scope.get('typeId') === null || scope.get('typeId') === '*')) {
+                            scope.get('ajax').request('/health-analytics-api/concepts', {data: {prefix: param}})
+                                .then((result) => {
+                                var list = {};
+                                var any = {};
+                                any.fsn = "Any";
+                                any.id = "*";
+                                any.subset = true;
+                                var filteredSubsets = [];
+                                filteredSubsets.push(any);
+                                list.items= filteredSubsets.concat(result.items);
+                                scope.set('filteredList', list);
+                            });
+                        }
+                        else if(this.get('mrcmType') && this.get('parentId') === null || this.get('parentId') === '*'){
+                            this.get('ajax').request('/health-analytics-api/concepts?ecQuery=%3C%20410662002&limit=50')
+                                .then((result) => {
+                                var filteredAttrs = [];
+                                result.items.forEach(function(item){
+                                    if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
+                                        filteredAttrs.push(item);
+                                    }
+                                });
+                                var list = {};
+                                var any = {};
+                                any.fsn = "Any";
+                                any.id = "*";
+                                any.subset = true;
+                                var filteredSubsets = [];
+                                filteredSubsets.push(any);
+                                list.items= filteredSubsets.concat(filteredAttrs);
+                                console.log(list);
+                                this.set('filteredList', list);
+                            }).catch(function(response, jqXHR, payload) {
+                                console.log(response);
+                                if (isNotFoundError(error)) {
+                                  // handle 404 errors here
+                                  return;
+                                }
+
+                                if (isForbiddenError(error)) {
+                                    location.href = '/login?serviceReferer=' + encodeURI(location.href);
+                                  // handle 403 errors here
+                                  return;
+                                }
+
+                                if(isAjaxError(error)) {
+                                    console.log('broken');
+                                  // handle all other AjaxErrors here
+                                  return;
+                                }
+
+                                // other errors are handled elsewhere
+                                throw error;
+                              });
+                        }
+                        else if(this.get('mrcmTarget') && this.get('typeId') !== null && this.get('typeId') !== '*'){
+                            this.get('ajax').request('/mrcm/attribute-values/'+this.get('typeId')+'?termPrefix='+ param + '*&expand=fsn()&offset=0&limit=50')
+                                .then((result) => {
+                                var filteredAttrs = [];
+                                result.items.forEach(function(item){
+                                    item.fsn = item.fsn.term;
+                                    item.id = item.id;
+                                    if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
+                                        filteredAttrs.push(item);
+                                    }
+                                });
+                                var list = {};
+                                var any = {};
+                                any.fsn = "Any";
+                                any.id = "*";
+                                any.subset = true;
+                                var filteredSubsets = [];
+                                filteredSubsets.push(any);
+                                list.items= filteredSubsets.concat(filteredAttrs);
+                                this.set('filteredList', list);
+                            });
+                        }
+                        else if(this.get('mrcmType') && this.get('parentId')){
+                            this.get('ajax').request('/mrcm/domain-attributes?parentIds=' + this.get('parentId') + '&expand=fsn()&offset=0&limit=50')
+                                .then((result) => {
+                                var filteredAttrs = [];
+                                result.items.forEach(function(item){
+                                    item.fsn = item.fsn.term;
+                                    item.id = item.id;
+                                    if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
+                                        filteredAttrs.push(item);
+                                    }
+                                });
+                                var list = {};
+                                var any = {};
+                                any.fsn = "Any";
+                                any.id = "*";
+                                any.subset = true;
+                                var filteredSubsets = [];
+                                filteredSubsets.push(any);
+                                list.items= filteredSubsets.concat(filteredAttrs);
+                                console.log(list);
+                                this.set('filteredList', list);
+                            }).catch(function(error) {
+                                if (isForbiddenError(error)) {
+                                    location.href = '/login?serviceReferer=' + encodeURI(location.href);
+                                  // handle 403 errors here
+                                  return;
+                                }
+                                // other errors are handled elsewhere
+                                throw error;
+                              });
+                        }
+                };
             if(param.length >= 3)
                 {
-                    if(param !== "" && param !== '*' && this.get('mrcmType') === null && (this.get('typeId') === null || this.get('typeId') === '*')) {
-                        this.get('ajax').request('/health-analytics-api/concepts', {data: {prefix: param}})
-                            .then((result) => {
-                            var list = {};
-                            var any = {};
-                            any.fsn = "Any";
-                            any.id = "*";
-                            any.subset = true;
-                            var filteredSubsets = [];
-                            filteredSubsets.push(any);
-                            list.items= filteredSubsets.concat(result.items);
-                            this.set('filteredList', list);
-                        });
-                    }
-                    else if(this.get('mrcmType') && this.get('parentId') === null || this.get('parentId') === '*'){
-                        this.get('ajax').request('/health-analytics-api/concepts?ecQuery=%3C%20410662002&limit=200')
-                            .then((result) => {
-                            var filteredAttrs = [];
-                            result.items.forEach(function(item){
-                                if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
-                                    filteredAttrs.push(item);
-                                }
-                            });
-                            var list = {};
-                            var any = {};
-                            any.fsn = "Any";
-                            any.id = "*";
-                            any.subset = true;
-                            var filteredSubsets = [];
-                            filteredSubsets.push(any);
-                            list.items= filteredSubsets.concat(filteredAttrs);
-                            console.log(list);
-                            this.set('filteredList', list);
-                        }).catch(function(response, jqXHR, payload) {
-                            console.log(response);
-                            if (isNotFoundError(error)) {
-                              // handle 404 errors here
-                              return;
-                            }
-
-                            if (isForbiddenError(error)) {
-                                location.href = '/login?serviceReferer=' + encodeURI(location.href);
-                              // handle 403 errors here
-                              return;
-                            }
-
-                            if(isAjaxError(error)) {
-                                console.log('broken');
-                              // handle all other AjaxErrors here
-                              return;
-                            }
-
-                            // other errors are handled elsewhere
-                            throw error;
-                          });
-                    }
-                    else if(this.get('mrcmTarget') && this.get('typeId') !== null && this.get('typeId') !== '*'){
-                        this.get('ajax').request('/mrcm/attribute-values/'+this.get('typeId')+'?termPrefix='+ param + '*&expand=fsn()&offset=0&limit=50')
-                            .then((result) => {
-                            var filteredAttrs = [];
-                            result.items.forEach(function(item){
-                                item.fsn = item.fsn.term;
-                                item.id = item.id;
-                                if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
-                                    filteredAttrs.push(item);
-                                }
-                            });
-                            var list = {};
-                            var any = {};
-                            any.fsn = "Any";
-                            any.id = "*";
-                            any.subset = true;
-                            var filteredSubsets = [];
-                            filteredSubsets.push(any);
-                            list.items= filteredSubsets.concat(filteredAttrs);
-                            this.set('filteredList', list);
-                        });
-                    }
-                    else if(this.get('mrcmType') && this.get('parentId')){
-                        this.get('ajax').request('/mrcm/domain-attributes?parentIds=' + this.get('parentId') + '&expand=fsn()&offset=0&limit=50')
-                            .then((result) => {
-                            var filteredAttrs = [];
-                            result.items.forEach(function(item){
-                                item.fsn = item.fsn.term;
-                                item.id = item.id;
-                                if(item.fsn.toLowerCase().indexOf(param.toLowerCase()) !== -1){
-                                    filteredAttrs.push(item);
-                                }
-                            });
-                            var list = {};
-                            var any = {};
-                            any.fsn = "Any";
-                            any.id = "*";
-                            any.subset = true;
-                            var filteredSubsets = [];
-                            filteredSubsets.push(any);
-                            list.items= filteredSubsets.concat(filteredAttrs);
-                            console.log(list);
-                            this.set('filteredList', list);
-                        }).catch(function(error) {
-                            if (isForbiddenError(error)) {
-                                location.href = '/login?serviceReferer=' + encodeURI(location.href);
-                              // handle 403 errors here
-                              return;
-                            }
-                            // other errors are handled elsewhere
-                            throw error;
-                          });
-                    }
+                    clearTimeout(this.timeout);
+                    var scope = this;
+                    this.timeout = setTimeout(function () {
+                        run(scope);
+                    }, 2000);
                 }
             else {
                 this.set('filteredList').clear();
