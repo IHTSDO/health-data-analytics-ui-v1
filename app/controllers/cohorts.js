815f268ee112a7f1380ce8cf51feb28ce27f30ea
Kai Kewley <kaikewley@gmail.com>
2020-06-24 22:19:04 +0100

    HDAD-34 New API compatibility: update cohort criteria, fetch and statistical test.

--- app/controllers/cohorts.js
+++ app/controllers/cohorts.js
@@ -13,7 +13,7 @@ export default Ember.Controller.extend({
             });
     },
     toECL: function (conceptString) {
-        if (conceptString.indexOf('|') !== -1) {
+        if (conceptString != null && conceptString.length > 0 && conceptString.indexOf('|') !== -1) {
             // Has label
             // Strip and label and convert to descendants and self
             return "<<" + conceptString.split(" ")[0];
@@ -78,135 +78,125 @@ export default Ember.Controller.extend({
 
             let inclusionCriteria = this.get('model.inclusionCriteria');
 
-            let inclusionCriteriaArray = [];
-            let postData = null;
+            let postData = {
+                baseCriteria: {
+                    encounterCriteria: [
+                    ]
+                }
+            };
+
+            if (primaryExposure != null && primaryExposure.length > 0) {
+                if (primaryExposure.indexOf("<<") === -1 && primaryExposure.indexOf(">>") === -1) {
+                    primaryExposure = "<< " + primaryExposure;
+                }
+                postData.baseCriteria.encounterCriteria.push({
+                    "has": true,
+                    "conceptECL": primaryExposure
+                })
+            }
+
             if (Ember.isPresent(inclusionCriteria)) {
                 inclusionCriteria.forEach(function (item){
-                    var inclusionCriteriaData = {};
+                    var encounterCriteriaData = {};
                     if(item.ecl !== ""){
                         if(item.ecl.indexOf("<<") === -1 && item.ecl.indexOf(">>") === -1){
-                            inclusionCriteriaData.ecl = "<< " + item.ecl;
+                            encounterCriteriaData.conceptECL = "<< " + item.ecl;
                         }
                         else{
-                            inclusionCriteriaData.ecl = item.ecl;
+                            encounterCriteriaData.conceptECL = item.ecl;
                         }
                         if(item.includeDaysInPast !== null && item.includeDaysInPast !== undefined){
                             if(item.includeDaysInPast === '*'){
                                 Ember.set(item, 'includeDaysInFuture', -1);
                                 Ember.set(item, 'includeDaysInPast', -1);
                             }
-                            inclusionCriteriaData.includeDaysInPast = item.includeDaysInPast;
-                            inclusionCriteriaData.includeDaysInFuture = item.includeDaysInPast;
+                            encounterCriteriaData.withinDaysBeforePreviouslyMatchedEncounter = item.includeDaysInPast;
+                            encounterCriteriaData.withinDaysAfterPreviouslyMatchedEncounter = item.includeDaysInPast;
                         }
-                        inclusionCriteriaData.has = item.has;
-                        inclusionCriteriaArray.push(inclusionCriteriaData);
-
+                        encounterCriteriaData.has = item.has;
+                        postData.baseCriteria.encounterCriteria.push(encounterCriteriaData);
                     }
                 });
-                
             }
-            
-            console.log("fetch cohort for " + primaryExposureECL + " with inclusionCriteria" + inclusionCriteriaArray);
-            if(primaryExposure.indexOf("<<") === -1 && primaryExposure.indexOf(">>") === -1){
-                primaryExposure = "<< " + primaryExposure;
-            }
-            postData = {
-                    primaryCriterion:  {
-                        "ecl": primaryExposure
-                      },
-                    additionalCriteria: inclusionCriteriaArray
-                };
             
             if(this.get('model.gender') !== null && this.get('model.gender') !== undefined && this.get('model.gender').length !== 0){
-                postData.gender = this.get('model.gender').toUpperCase();
+                postData.baseCriteria.gender = this.get('model.gender').toUpperCase();
             }
             if(this.get('model.ageMin') !== null && this.get('model.ageMin') !== undefined){
-                postData.minAge = this.get('model.ageMin');
+                postData.baseCriteria.minAgeNow = this.get('model.ageMin');
             }
             if(this.get('model.ageMax') !== null && this.get('model.ageMax') !== undefined){
-                postData.maxAge = this.get('model.ageMax');
+                postData.baseCriteria.maxAgeNow = this.get('model.ageMax');
             }
             let testOutcome = this.get('model.testOutcome');
             let testVariable = this.get('model.testVariable');
             var loading = this.get('loading');
             var error = this.get('error');
             if (Ember.isPresent(testOutcome)) {
-                if(testOutcome.includeDaysInFuture === '*'){
-                                Ember.set(testOutcome, 'includeDaysInFuture', -1);
-                            }
-                if(testVariable.includeDaysInFuture === '*'){
-                                Ember.set(testVariable, 'includeDaysInFuture', -1);
-                            }
-                if(testOutcome.ecl.indexOf("<<") === -1 && testOutcome.ecl.indexOf(">>") === -1){
-                    Ember.set(testOutcome, 'ecl', "<< " + testOutcome.ecl);
+
+                var treatmentCriterion = {
+
                 }
-                if(testVariable.ecl.indexOf("<<") === -1 && testVariable.ecl.indexOf(">>") === -1){
-                    Ember.set(testVariable, 'ecl', "<< " + testVariable.ecl);
+                if (testVariable.includeDaysInFuture != '*') {
+                    treatmentCriterion.withinDaysBeforePreviouslyMatchedEncounter = testVariable.includeDaysInFuture;
                 }
-                postData.testOutcome = testOutcome;
-                postData.testVariable = testVariable;
-                var data = 
-                    this.get('ajax').post('/health-analytics-api/cohorts/statistical-test', {
+                treatmentCriterion.conceptECL = testVariable.ecl.indexOf("<<") === -1 && testVariable.ecl.indexOf(">>") === -1 ? "<<" + testVariable.ecl : testVariable.ecl;
+
+                var negativeOutcomeCriterion = {
+
+                }
+                if (testOutcome.includeDaysInFuture != '*') {
+                    negativeOutcomeCriterion.withinDaysBeforePreviouslyMatchedEncounter = testOutcome.includeDaysInFuture;
+                }
+                negativeOutcomeCriterion.conceptECL = testOutcome.ecl.indexOf("<<") === -1 && testOutcome.ecl.indexOf(">>") === -1 ? "<<" + testOutcome.ecl : testOutcome.ecl;
+
+
+                postData.treatmentCriterion = treatmentCriterion;
+                postData.negativeOutcomeCriterion = negativeOutcomeCriterion;
+                this.get('ajax').post(
+                    '/health-analytics-api/statistical-correlation-report',
+                    {
                         contentType: 'application/json; charset=utf-8',
-                        data: JSON.stringify(postData)})
-                        .then((result) => {
-                            this.set('loading', false);
-                            this.set('model.cohortData', result);
-                            if(testOutcome.includeDaysInFuture === -1){
-                                Ember.set(testOutcome, 'includeDaysInFuture', '*');
-                            }
-                            if(testVariable.includeDaysInFuture === -1){
-                                Ember.set(testVariable, 'includeDaysInFuture', '*');
-                            }
-                        }).catch(function(error) {
-                            if (isServerError(error)) {
-                                loading = false;
-                                error = 'There has been a problem with your request - please check your input.';
-                              // handle 5xx errors here
-                              return;
-                            }
-                            // other errors are handled elsewhere
-                            throw error;
-                          });
-            }
-            else{
-                var data = 
-                    this.get('ajax').post('/health-analytics-api/cohorts/select', {
+                        data: JSON.stringify(postData)
+                    })
+                    .then((result) => {
+                        this.set('loading', false);
+                        this.set('model.cohortData', result);
+                    }).catch(function(error) {
+                        if (isServerError(error)) {
+                            loading = false;
+                            error = 'There has been a problem with your request - please check your input.';
+                            // handle 5xx errors here
+                            return;
+                        }
+                        // other errors are handled elsewhere
+                        throw error;
+                    });
+            } else {
+                this.get('ajax').post(
+                    '/health-analytics-api/cohorts/select',
+                    {
                         contentType: 'application/json; charset=utf-8',
-                        data: JSON.stringify(postData)})
-                        .then((result) => {
-                            inclusionCriteria.forEach(function (item){
-                                var inclusionCriteriaData = {};
-                                console.log(item);
-                                    if(item.includeDaysInPast !== null && item.includeDaysInPast !== undefined){
-                                        if(item.includeDaysInPast === -1){
-                                            Ember.set(item, 'includeDaysInFuture', '*');
-                                            Ember.set(item, 'includeDaysInPast', '*');
-                                        }
-                                        inclusionCriteriaData.includeDaysInPast = item.includeDaysInPast;
-                                        inclusionCriteriaData.includeDaysInFuture = item.includeDaysInPast;
-                                    }
-                                    inclusionCriteriaData.has = item.has;
-                                    inclusionCriteriaArray.push(inclusionCriteriaData);
-                            });
-                            
-                            this.set('loading', false); 
-                            this.set('model.cohortData', result);
-                        }).catch(function(error) {
-                            if (isServerError(error)) {
-                                Ember.set(loading, false);
-                                Ember.set(error, 'There has been a problem with your request - please check your input.');
-                              // handle 5xx errors here
-                              return;
-                            }
-                            // other errors are handled elsewhere
-                            throw error;
-                          });
+                        data: JSON.stringify(postData.baseCriteria)
+                    })
+                    .then((result) => {
+                        result.totalElementsFormatted = result.totalElements.toLocaleString();
+                        this.set('loading', false);
+                        this.set('model.cohortData', result);
+                    })
+                    .catch(function(error) {
+                        if (isServerError(error)) {
+                            Ember.set(loading, false);
+                            Ember.set(error, 'There has been a problem with your request - please check your input.');
+                            // handle 5xx errors here
+                            return;
+                        }
+                        // other errors are handled elsewhere
+                        throw error;
+                    });
                 this.set('loading', loading); 
-            this.set('error', error);
+                this.set('error', error);
             }
-            
-            
         },
         updateGender(){
             console.log(this);
